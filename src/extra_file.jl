4b883ad3a0b4a4a0e44805586278c74e82f5914a
Long <ltran0325@gmail.com>
2022-05-22 21:27:20 -0700

    testing branch
    test branch feature

--- src/extra_file.jl
+++ src/extra_file.jl
@@ -3,6 +3,20 @@
         my_f(x,y)
         Return 2*x+3*y
 
+        Linear function body
         Enter ?my_f to use Julia's help system to view Docstring in REPL
 """
 my_f(x,y) = 2*x+3*y
+
+"""
+    Docstring:
+        my_f2(x,y)
+        Return true if println passes
+
+        Normal function body
+        Enter ?my_f2 to use Julia's help system to view Docstring in REPL
+"""
+function my_f2(x,y)::bool   
+    println(x, "and", y)
+    return true
+end
