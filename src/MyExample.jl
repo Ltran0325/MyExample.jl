4bee01a9ec4aa7d350ce8cd6300429451251155e
Long <ltran0325@gmail.com>
2022-05-22 20:42:22 -0700

    Added extra_file

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,8 @@
 module MyExample
 
 # Write your package code here.
+include("extra_file.jl")
 
-end
+export my_f
+
+end # module
---
+++ src/extra_file.jl
@@ -0,0 +1,8 @@
+"""
+    Docstring:
+        my_f(x,y)
+        Return 2*x+y
+
+        Enter ?my_f to use Julia's help system to view Docstring in REPL
+"""
+my_f(x,y) = 2*x+y
