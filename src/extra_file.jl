ca6a6c209400e67c4df4d294066bbde62854e991
Long <ltran0325@gmail.com>
2022-05-22 20:48:09 -0700

    Edited my_f
    Changed linear function from
    2*x + y
    to
    2*x + 3*y

--- src/extra_file.jl
+++ src/extra_file.jl
@@ -1,8 +1,8 @@
 """
     Docstring:
         my_f(x,y)
-        Return 2*x+y
+        Return 2*x+3*y
 
         Enter ?my_f to use Julia's help system to view Docstring in REPL
 """
-my_f(x,y) = 2*x+y
+my_f(x,y) = 2*x+3*y
